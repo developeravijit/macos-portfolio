07c66ebec9eff3a8856137b6766fa1c1635f0796
developeravijit <roysidharth1996@gmail.com>
2026-01-08 03:59:28 +0530

    added updated resume

--- src/windows/Resume.jsx
+++ src/windows/Resume.jsx
@@ -2,7 +2,7 @@ import WindowControls from "#components/WindowControls";
 import windowWrapper from "#hoc/windowWrapper.jsx";
 import { Download } from "lucide-react";
 import { Document, Page, pdfjs } from "react-pdf";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
@@ -12,18 +12,20 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 ).toString();
 
 const Resume = () => {
-  const pdfPath = `${import.meta.env.BASE_URL}files/resume.pdf`;
+  const pdfPath = `${import.meta.env.BASE_URL}files/Avijit_Roy_Resume.pdf`;
   const [numPages, setNumPages] = useState(null);
   const [scale, setScale] = useState(1);
 
-  let lastTap = 0;
+  const lastTapRef = useRef(0);
 
   const handleDoubleTap = () => {
     const now = Date.now();
-    if (now - lastTap < 300) {
+
+    if (now - lastTapRef.current < 300) {
       setScale((prev) => (prev > 1 ? 1 : 1.6));
     }
-    lastTap = now;
+
+    lastTapRef.current = now;
   };
 
   const zoomIn = () => setScale((prev) => Math.min(prev + 0.1, 2));
